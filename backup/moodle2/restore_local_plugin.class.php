9da77cf88f98db04773effc27dc2fa92a6025ed1
Sam Chaffee <sam@moodlerooms.com>
2012-04-12 16:57:52 -0400

    MDL-17444: Backup hooks for local plugin

--- backup/moodle2/restore_local_plugin.class.php
+++ backup/moodle2/restore_local_plugin.class.php
@@ -24,23 +24,7 @@
 
 /**
  * Class extending standard restore_plugin in order to implement some
- * helper methods related with the plagiarism plugins
+ * helper methods related with local plugins
  *
- * TODO: Finish phpdocs
  */
-abstract class restore_plagiarism_plugin extends restore_plugin {
-    public function define_plugin_structure($connectionpoint) {
-        global $CFG;
-        if (!$connectionpoint instanceof restore_path_element) {
-            throw new restore_step_exception('restore_path_element_required', $connectionpoint);
-        }
-
-        //check if enabled at site level and plugin is enabled.
-        require_once($CFG->libdir . '/plagiarismlib.php');
-        $enabledplugins = plagiarism_load_available_plugins();
-        if (!array_key_exists($this->pluginname, $enabledplugins)) {
-            return array();
-        }
-        return parent::define_plugin_structure($connectionpoint);
-    }
-}
+abstract class restore_local_plugin extends restore_plugin {}

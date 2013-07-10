aef9bee270e9b4adb6c5321abfb578175238b8c0
Mark Nielsen <mark@moodlerooms.com>
2013-07-10 09:37:04 -0700

    INT-4475: Fixing installation config defaults
    On fresh install, ensure that configs upgrade
    so legacy outcome system is not disabled
    if it was enabled.  Upgrades already handled
    this.

--- admin/tool/outcome/db/install.php
+++ admin/tool/outcome/db/install.php
@@ -1,6 +1,15 @@
 <?php
 
 function xmldb_tool_outcome_install() {
+    global $CFG;
+
     // Patched quiz db/events.php but did not bump quiz version
     events_update_definition('mod_quiz');
+
+    if (!empty($CFG->enableoutcomes)) {
+        set_config('enable', 1, 'core_outcome');
+    } else {
+        set_config('enable', 0, 'core_outcome');
+    }
+    set_config('core_outcome_enable', 0);
 }
